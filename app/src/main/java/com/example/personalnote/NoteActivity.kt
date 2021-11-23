390f22a6aead3233de5389c6824734564984ae0f
basheer574 <spyderme99100@gmail.com>
2021-11-23 12:18:45 +0300

    add libraries

--- app/src/main/java/com/example/personalnote/NoteActivity.kt
+++ app/src/main/java/com/example/personalnote/NoteActivity.kt
@@ -3,7 +3,7 @@ package com.example.personalnote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
