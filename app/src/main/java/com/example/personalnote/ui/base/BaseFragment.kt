7ec714769f48510d48cb4d522539764a7681bfde
basheer <spyderme99100@gmail.com>
2021-11-25 18:48:23 +0300

    add base fragment

--- app/src/main/java/com/example/personalnote/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/personalnote/ui/base/BaseFragment.kt
@@ -1,4 +1,37 @@
 package com.example.personalnote.ui.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId: Int) : Fragment() {
+
+    abstract val viewModel: BaseViewModel
+
+    private lateinit var _binding: VDB
+    val binding: VDB get() = _binding
+    abstract val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater, fragmentLayoutId, container, false)
+        _binding.setVariable(BR.viewModel, viewModel)
+        _binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    abstract fun setupView()
 }
