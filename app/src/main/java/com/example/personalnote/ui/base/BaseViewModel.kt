a9dfb24cd0f6e936224b782a02c8f725987792b3
basheer <spyderme99100@gmail.com>
2021-11-25 18:52:54 +0300

    add base view model

--- app/src/main/java/com/example/personalnote/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/example/personalnote/ui/base/BaseViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.personalnote.ui.base
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.cancel
+
+class BaseViewModel : ViewModel(){
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.cancel()
+    }
 }
