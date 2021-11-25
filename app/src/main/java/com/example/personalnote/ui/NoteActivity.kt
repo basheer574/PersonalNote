98d5666ab7c47dc6e0aeabf8efa4d22b6e69563b
basheer <spyderme99100@gmail.com>
2021-11-25 18:21:01 +0300

    modify gradle

--- app/src/main/java/com/example/personalnote/ui/NoteActivity.kt
+++ app/src/main/java/com/example/personalnote/ui/NoteActivity.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import com.example.personalnote.R
 
 class NoteActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note)
     }
 }
