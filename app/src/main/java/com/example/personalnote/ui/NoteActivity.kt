415a06bf8005ce4bf18560e5fa96a6bc8c297212
basheer <spyderme99100@gmail.com>
2021-11-25 18:17:38 +0300

    add constants

--- app/src/main/java/com/example/personalnote/ui/NoteActivity.kt
+++ app/src/main/java/com/example/personalnote/ui/NoteActivity.kt
@@ -1,7 +1,8 @@
-package com.example.personalnote
+package com.example.personalnote.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.personalnote.R
 
 class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/personalnote/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.personalnote.utils
+
+
+
+object Constants {
+    const val DATABASE_NAME = "Notes"
+    const val TABLE_NAME = "Notes_table"
+    const val VERSION = 1
+}
